a38f29599eb8dc9e471d25c31cf17e2e8e1d9953
Basile Aelterman <basile@dennenboom.be>
2026-08-21 22:02:43 +0200

    feat: add SNTP functionality with initialization function and update CMake configuration

---
+++ include/sntp.h
@@ -0,0 +1,21 @@
+#ifndef SNTP_H
+#define SNTP_H
+
+#include "esp_err.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Initialize the time.
+ * 
+ * @return esp_err_t
+ */
+esp_err_t sntp_init();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // SNTP_H
